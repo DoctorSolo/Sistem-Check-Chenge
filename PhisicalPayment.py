44db185e846180318d28650cad7b6bddf9f1522c
Miguel <dasilvamigueleduardo@gmail.com>
2024-08-15 13:57:36 -0300

    create checkChenge

--- PhisicalPayment.py
+++ PhisicalPayment.py
@@ -1,3 +1,15 @@
-Class Payment:
-    def __init__():
-        pass
+class PhisicalPayment():
+    def __init__(value, money, price):
+        self.DL_ = value # DL is a play money, valores is a list
+        
+        self.money = money
+        self.price = price
+
+    
+    def checkChenge(self, money, price) -> bool:
+        money_t = sum(money)
+        if money_t < price:
+            return False
+        
+        elif money_t == price:
+            self.DL_ =
