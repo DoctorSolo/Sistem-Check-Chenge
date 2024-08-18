d255ebde85574f57af1b66ab7a9cb956362bbb17
Miguel <dasilvamigueleduardo@gmail.com>
2024-08-18 18:24:39 -0300

    create - class product

---
+++ Product.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+class Product:
+    dictProduct = {
+        "Limpeza": {"Alcool 70": 20.00, 
+                    "Agua Sanitaria": 30.00,
+                    "Pano Panda": 50.00},
+        "Comida": {"Pão": 0.50,
+                   "Queijo queijo": 50.00,
+                   "Peixe Pirata": 30.50,
+                   "Frango": 35.00}
+    }
+    
+    
+    def __init__(self) -> None:
+        pass
+    
+    
+    def showProduct(self):
+        pd.DateOffset(self.dictProduct)
+    
+    
+    def choiseP(self, category, product):
+        return self.dictProduct[category][product]
